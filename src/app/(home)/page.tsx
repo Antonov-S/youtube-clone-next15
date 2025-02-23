746a8d3c7ec40f00f6148c05e5fdeae456005401
Antonov-S <anto1nov1@gmail.com>
2025-02-23 23:17:52 +0200

    Adding TRPC setup

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,19 @@
-function Page() {
-  return <div className="">I will load videos in the future!</div>;
-}
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
-export default Page;
+import { PageClient } from "./client";
+import { HydrateClient, trpc } from "@/trpc/server";
+
+export default async function Home() {
+  void trpc.hello.prefetch({ text: "Svetlozar2S" });
+
+  return (
+    <HydrateClient>
+      <ErrorBoundary fallback={<div>Something went wrong</div>}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <PageClient />
+        </Suspense>
+      </ErrorBoundary>
+    </HydrateClient>
+  );
+}
