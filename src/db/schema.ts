bd55c445dbeecbe613982f8f556e47a2eef931bf
Antonov-S <anto1nov1@gmail.com>
2025-03-09 18:01:45 +0200

    Create Categories datatable and seeding script

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -19,3 +19,15 @@ export const users = pgTable(
   },
   t => [uniqueIndex("clerk_id_idx").on(t.clerk_id)]
 );
+
+export const categories = pgTable(
+  "categories",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: text("name").unique().notNull(),
+    description: text("description"),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull()
+  },
+  t => [uniqueIndex("name_inx").on(t.name)]
+);
