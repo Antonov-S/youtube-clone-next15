375c021bd5b91d9e426d7ef809601df4b6d94e2d
Antonov-S <anto1nov1@gmail.com>
2025-03-13 15:59:24 +0200

    Implement add video record creation

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,12 @@
-import { createTRPCRouter } from "../init";
 import { categoriesRouter } from "@/modules/categories/server/procedures";
 import { studioRauter } from "@/modules/studio/server/procedures";
+import { videosRauter } from "@/modules/videos/server/procedures";
+
+import { createTRPCRouter } from "../init";
 
 export const appRouter = createTRPCRouter({
   studio: studioRauter,
+  videos: videosRauter,
   categories: categoriesRouter
 });
 // export type definition of API
