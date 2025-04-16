9f46675a3e88b717b4df8ee884f1666f09f4feb4
Antonov-S <anto1nov1@gmail.com>
2025-04-16 03:53:26 +0300

    Connect VideoReactions component with new API

--- src/modules/videos/ui/components/video-reactions.tsx
+++ src/modules/videos/ui/components/video-reactions.tsx
@@ -1,9 +1,12 @@
+import { toast } from "sonner";
+import { useClerk } from "@clerk/nextjs";
 import { ThumbsDownIcon, ThumbsUpIcon } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 
+import { trpc } from "@/trpc/client";
 import { VideoGetOneOutput } from "../../types";
 
 interface VideoReactionsProps {
@@ -19,9 +22,42 @@ export const VideoReactions = ({
   dislikes,
   viewerReaction
 }: VideoReactionsProps) => {
+  const clerk = useClerk();
+  const utils = trpc.useUtils();
+
+  const like = trpc.videoReactions.like.useMutation({
+    onSuccess: () => {
+      utils.videos.getOne.invalidate({ id: videoId });
+      // TODO: Invalidate "liked" playlist letter
+    },
+    onError: error => {
+      toast.error("Something went wrong");
+
+      if (error.data?.code === "UNAUTHORIZED") {
+        clerk.openSignIn();
+      }
+    }
+  });
+
+  const dislike = trpc.videoReactions.dislike.useMutation({
+    onSuccess: () => {
+      utils.videos.getOne.invalidate({ id: videoId });
+      // TODO: Invalidate "disliked" playlist letter
+    },
+    onError: error => {
+      toast.error("Something went wrong");
+
+      if (error.data?.code === "UNAUTHORIZED") {
+        clerk.openSignIn();
+      }
+    }
+  });
+
   return (
     <div className="flex items-center flex-none">
       <Button
+        onClick={() => like.mutate({ videoId })}
+        disabled={like.isPending || dislike.isPending}
         className="rounded-l-full rounded-r-none gap-2 pr-4"
         variant="secondary"
       >
@@ -32,6 +68,8 @@ export const VideoReactions = ({
       </Button>
       <Separator orientation="vertical" className="h-7" />
       <Button
+        onClick={() => dislike.mutate({ videoId })}
+        disabled={like.isPending || dislike.isPending}
         className="rounded-l-none rounded-r-full pl-3"
         variant="secondary"
       >
