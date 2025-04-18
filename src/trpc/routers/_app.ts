bd76212df475539e24d88b277b4c92bc42af8291
Antonov-S <anto1nov1@gmail.com>
2025-04-19 00:54:01 +0300

    Create subscriptions procedures

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,6 +2,7 @@ import { categoriesRouter } from "@/modules/categories/server/procedures";
 import { studioRauter } from "@/modules/studio/server/procedures";
 import { videosRauter } from "@/modules/videos/server/procedures";
 import { videoViewsRauter } from "@/modules/video-views/server/procedures";
+import { subscriptionRauter } from "@/modules/subscriptions/server/procedures";
 import { videoReactionsRauter } from "@/modules/video-reactions/server/procedures";
 
 import { createTRPCRouter } from "../init";
@@ -11,6 +12,7 @@ export const appRouter = createTRPCRouter({
   videos: videosRauter,
   categories: categoriesRouter,
   videoViews: videoViewsRauter,
+  subscriptions: subscriptionRauter,
   videoReactions: videoReactionsRauter
 });
 // export type definition of API
