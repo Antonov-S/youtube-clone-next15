41d814532174a7f8729dcb1953f61d4a009f8eb8
Antonov-S <anto1nov1@gmail.com>
2025-04-19 22:32:55 +0300

    Create comments procedures

---
+++ src/modules/comments/server/procedures.ts
@@ -0,0 +1,48 @@
+import { z } from "zod";
+import { eq, and, or, lt, desc } from "drizzle-orm";
+
+import {
+  baseProcedure,
+  createTRPCRouter,
+  protectedProcedure
+} from "@/trpc/init";
+
+import { db } from "@/db";
+import { comments } from "@/db/schema";
+
+export const commentsRauter = createTRPCRouter({
+  create: protectedProcedure
+    .input(
+      z.object({
+        videoId: z.string().uuid(),
+        value: z.string()
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      const { videoId, value } = input;
+      const { id: userId } = ctx.user;
+
+      const [createdComment] = await db
+        .insert(comments)
+        .values({ userId, videoId, value })
+        .returning();
+
+      return createdComment;
+    }),
+  getMany: baseProcedure
+    .input(
+      z.object({
+        videoId: z.string().uuid()
+      })
+    )
+    .query(async ({ input }) => {
+      const { videoId } = input;
+
+      const data = await db
+        .select()
+        .from(comments)
+        .where(eq(comments.videoId, videoId));
+
+      return data;
+    })
+});
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,6 +1,7 @@
-import { categoriesRouter } from "@/modules/categories/server/procedures";
 import { studioRauter } from "@/modules/studio/server/procedures";
 import { videosRauter } from "@/modules/videos/server/procedures";
+import { commentsRauter } from "@/modules/comments/server/procedures";
+import { categoriesRouter } from "@/modules/categories/server/procedures";
 import { videoViewsRauter } from "@/modules/video-views/server/procedures";
 import { subscriptionRauter } from "@/modules/subscriptions/server/procedures";
 import { videoReactionsRauter } from "@/modules/video-reactions/server/procedures";
@@ -10,6 +11,7 @@ import { createTRPCRouter } from "../init";
 export const appRouter = createTRPCRouter({
   studio: studioRauter,
   videos: videosRauter,
+  comments: commentsRauter,
   categories: categoriesRouter,
   videoViews: videoViewsRauter,
   subscriptions: subscriptionRauter,
