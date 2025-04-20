a3914f3514c146bbea1a43ed516c230e1b28463e
Antonov-S <anto1nov1@gmail.com>
2025-04-20 20:25:38 +0300

    Modify comments getMany procedure

--- src/modules/comments/server/procedures.ts
+++ src/modules/comments/server/procedures.ts
@@ -32,11 +32,18 @@ export const commentsRauter = createTRPCRouter({
   getMany: baseProcedure
     .input(
       z.object({
-        videoId: z.string().uuid()
+        videoId: z.string().uuid(),
+        cursor: z
+          .object({
+            id: z.string().uuid(),
+            updatedAt: z.date()
+          })
+          .nullish(),
+        limit: z.number().min(1).max(100)
       })
     )
     .query(async ({ input }) => {
-      const { videoId } = input;
+      const { videoId, cursor, limit } = input;
 
       const data = await db
         .select({
@@ -44,9 +51,39 @@ export const commentsRauter = createTRPCRouter({
           user: users
         })
         .from(comments)
-        .where(eq(comments.videoId, videoId))
-        .innerJoin(users, eq(comments.userId, users.id));
+        .where(
+          and(
+            eq(comments.videoId, videoId),
+            cursor
+              ? or(
+                  lt(comments.updatedAt, cursor.updatedAt),
+                  and(
+                    eq(comments.updatedAt, cursor.updatedAt),
+                    lt(comments.id, cursor.id)
+                  )
+                )
+              : undefined
+          )
+        )
+        .innerJoin(users, eq(comments.userId, users.id))
+        .orderBy(desc(comments.updatedAt), desc(comments.id))
+        .limit(limit + 1);
 
-      return data;
+      const hasMore = data.length > limit;
+      //   Remove the last item if there is more data
+      const items = hasMore ? data.slice(0, -1) : data;
+      //   Set the next cursor to the last item if there is more data
+      const lastItem = items[items.length - 1];
+      const nextCursor = hasMore
+        ? {
+            id: lastItem.id,
+            updatedAt: lastItem.updatedAt
+          }
+        : null;
+
+      return {
+        items,
+        nextCursor
+      };
     })
 });
