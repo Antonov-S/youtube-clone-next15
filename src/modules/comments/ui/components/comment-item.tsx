3084454dd7af1635f04015877380ea6e6c6dbb71
Antonov-S <anto1nov1@gmail.com>
2025-04-22 04:33:08 +0300

    Create comment reactions UI

--- src/modules/comments/ui/components/comment-item.tsx
+++ src/modules/comments/ui/components/comment-item.tsx
@@ -2,8 +2,15 @@ import Link from "next/link";
 import { toast } from "sonner";
 import { formatDistanceToNow } from "date-fns";
 import { useAuth, useClerk } from "@clerk/nextjs";
-import { MessageSquareIcon, MoreVerticalIcon, Trash2Icon } from "lucide-react";
+import {
+  MessageSquareIcon,
+  MoreVerticalIcon,
+  ThumbsDownIcon,
+  ThumbsUpIcon,
+  Trash2Icon
+} from "lucide-react";
 
+import { cn } from "@/lib/utils";
 import { trpc } from "@/trpc/client";
 import { Button } from "@/components/ui/button";
 import { UserAvatar } from "@/components/user-avatar";
@@ -61,7 +68,30 @@ export const CommentItem = ({ comment }: CommentItemProps) => {
             </div>
           </Link>
           <p className="text-sm">{comment.value}</p>
-          {/* TODO: Reactions */}
+          <div className="flex items-center gap-2 mt-1">
+            <div className="flex items-center">
+              <Button
+                disabled={false}
+                variant="ghost"
+                size="icon"
+                className="size-8"
+                onClick={() => {}}
+              >
+                <ThumbsUpIcon className={cn()} />
+              </Button>
+              <span className="text-xs text-muted-foreground">0</span>
+              <Button
+                disabled={false}
+                variant="ghost"
+                size="icon"
+                className="size-8"
+                onClick={() => {}}
+              >
+                <ThumbsDownIcon className={cn()} />
+              </Button>
+              <span className="text-xs text-muted-foreground">0</span>
+            </div>
+          </div>
         </div>
         <DropdownMenu modal={false}>
           <DropdownMenuTrigger asChild>
