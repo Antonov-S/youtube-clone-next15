b21908ac391684682e7a01e3a4c0a452637db94c
Antonov-S <anto1nov1@gmail.com>
2025-05-02 04:22:04 +0300

    Adding skeletons to all components in Suggestion section

--- src/modules/videos/ui/components/video-thumbnail.tsx
+++ src/modules/videos/ui/components/video-thumbnail.tsx
@@ -3,12 +3,22 @@ import Image from "next/image";
 import { formatDuration } from "@/lib/utils";
 import { THUBNAIL_FALLBACK } from "../../constants";
 
+import { Skeleton } from "@/components/ui/skeleton";
+
 interface VideoThumbnailProps {
   imageUrl?: string | null;
   previewUrl?: string | null;
   title: string;
   duration: number;
 }
+
+export const VideoThumbnailSkeleton = () => {
+  return (
+    <div className="relative w-full overflow-hidden rounded-xl aspect-video">
+      <Skeleton className="size-full" />
+    </div>
+  );
+};
 
 export const VideoThumbnail = ({
   imageUrl,
