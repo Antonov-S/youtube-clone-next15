6c62b20c533ffe5fe61270701be245b6912e48fa
Antonov-S <anto1nov1@gmail.com>
2025-05-13 02:58:51 +0300

    Add Subscriptions page, getManySubscribed procedure, prefetch, render the videos and revalidate when subscribe/unsubscribe

--- src/modules/home/ui/components/home-sidebar/main-section.tsx
+++ src/modules/home/ui/components/home-sidebar/main-section.tsx
@@ -14,8 +14,8 @@ import { FlameIcon, HomeIcon, PlaySquareIcon } from "lucide-react";
 const items = [
   { title: "Home", url: "/", icon: HomeIcon },
   {
-    title: "Subscriptions",
-    url: "/feed/subscriptions",
+    title: "Subscribed",
+    url: "/feed/subscribed",
     icon: PlaySquareIcon,
     auth: true
   },
