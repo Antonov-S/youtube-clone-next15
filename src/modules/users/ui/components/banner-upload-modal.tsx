e8a1bad409e928e517457ef0246e364491063339
Antonov-S <anto1nov1@gmail.com>
2025-05-26 03:26:48 +0300

    Create BannerUploadModal

---
+++ src/modules/users/ui/components/banner-upload-modal.tsx
@@ -0,0 +1,36 @@
+import { trpc } from "@/trpc/client";
+
+import { ResponsiveModal } from "@/components/responsive-dialog";
+import { UploadDropzone } from "@/lib/uploadthing";
+
+interface BannerUploadModalProps {
+  userId: string;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export const BannerUploadModal = ({
+  userId,
+  open,
+  onOpenChange
+}: BannerUploadModalProps) => {
+  const utils = trpc.useUtils();
+
+  const onUploadComplete = () => {
+    utils.users.getOne.invalidate({ id: userId });
+    onOpenChange(false);
+  };
+
+  return (
+    <ResponsiveModal
+      title="Upload a banner"
+      open={open}
+      onOpenChange={onOpenChange}
+    >
+      <UploadDropzone
+        endpoint="bannerUploader"
+        onClientUploadComplete={onUploadComplete}
+      />
+    </ResponsiveModal>
+  );
+};
